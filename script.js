d24efe3b110b279ddb83895975622bdf0d0c6e75
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-04-02 04:41:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
     //Write your code here
-  
+	let tableElement = document.getElementById("sampleTable");
+	let column1 = document.createElement("td");
+	column1.textContent = "New Cell1";
+	let column2 = document.createElement("td");
+	column2.textContent = "New Cell2";
+	
+	let row = tableElement.insertRow(0)
+
+	row.appendChild(column1);
+	row.appendChild(column2);  
   
 }
